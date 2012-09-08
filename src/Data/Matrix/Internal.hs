1d1ca94dcc016a8e543795b670a166e403cdb72d
Johannes Weiß <weiss@tux4u.de>
2012-09-08 13:46:20 +0200

    TRIVIAL: Setup

---
+++ src/Data/Matrix/Internal.hs
@@ -0,0 +1,22 @@
+{-
+ # This file is part of matrix-arbitrary.                                      #
+ #                                                                             #
+ # matrix-arbitrary is free software: you can redistribute it and/or modify    #
+ # it under the terms of the GNU Lesser General Public License as published by #
+ # the Free Software Foundation, either version 3 of the License, or           #
+ # (at your option) any later version.                                         #
+ #                                                                             #
+ # matrix-arbitrary is distributed in the hope that it will be useful,         #
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+ # GNU Lesser General Public License for more details.                         #
+ #                                                                             #
+ # A copy of the GNU Lesser General Public License resides in the `LICENSE`    #
+ # file distributed along with matrix-arbitrary.                               #
+ #                                                                             #
+ # Copyright 2012, Johannes Weiß <weiss@tux4u.de>                              #
+-}
+
+module Data.Matrix.Internal () where
+
+-- vim: set fileencoding=utf8 :
